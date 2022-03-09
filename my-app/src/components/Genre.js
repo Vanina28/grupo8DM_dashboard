2a6f407f074a4e01f9e2cd8e8354e54c189becaf
Vanina28 <vanimeni2002@gmail.com>
2022-03-09 11:50:34 -0300

    mas componentes

---
+++ my-app/src/components/Genre.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {Link} from 'react-router-dom'
+
+
+
+
+function Genre(props) {
+
+    return (
+        <>
+        
+        <div className="col-lg-6 mb-4">
+		    <div className="card bg-dark text-white shadow" style={{marginLeft: 10, marginRight: 8,}}>
+			 <div className="card-body">
+					{props.name}		
+             					
+			 </div>
+			</div>
+        </div>
+        
+
+
+        </>
+
+    )
+
+
+
+
+}
+
+export default Genre
